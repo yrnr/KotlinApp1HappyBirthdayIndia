4f0e3dea01216d11007361eb5afedf613445ad2d
yrnr <iamramuhere@gmail.com>
2020-07-07 23:32:17 +0530

    String resource with placeholder - assignment should be lifted out of if statement in Kotlin

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -22,40 +22,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var daysToBirthday = ""
         findViewById<Button>(R.id.buttonView).setOnClickListener {
-            daysToBirthday = getDays(findViewById<EditText>(R.id.editTextView).text.toString())
-            findViewById<EditText>(R.id.editTextView).visibility = View.GONE
-            findViewById<Button>(R.id.buttonView).visibility = View.GONE
-            findViewById<ImageView>(R.id.imageView).visibility = View.VISIBLE
             //Hide keyboard
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken, 0)
-            // Make the display string
-            val days1 = getResources().getString(R.string.days1)
-            val days2 = getResources().getString(R.string.days2)
-            findViewById<TextView>(R.id.textView).text = days1 + " " + daysToBirthday + days2
+            // Use the date-of-birth text entered, to know how far away is the next birthday
+            daysToBirthday = getDays(findViewById<EditText>(R.id.editTextView).text.toString())
+            // Vanish the editTextView and buttonView
+            findViewById<EditText>(R.id.editTextView).visibility = View.GONE
+            findViewById<Button>(R.id.buttonView).visibility = View.GONE
+            // Show the imageView
+            findViewById<ImageView>(R.id.imageView).visibility = View.VISIBLE
+            // Show the display string
+            findViewById<TextView>(R.id.textView).text = String.format(
+                getResources().getString(R.string.days1), daysToBirthday)
         }
     }
 
-    fun getDays(string: String): String {
-        val today = LocalDateTime.now()
-        val birth = LocalDateTime.parse(string + "T00:00:00.0400")
-        var nextBirthdayYear = 0
-        var nextBirthday = today
-        if (
-            ((today.monthValue == birth.monthValue) && (today.dayOfMonth >= birth.dayOfMonth))
-            || (today.monthValue > birth.monthValue)) { nextBirthdayYear = today.year + 1 }
-        else { nextBirthdayYear = today.year }
-        var birthMonthStr = ""
-        var birthDayOfMonthStr = ""
-        if (birth.monthValue < 10) { birthMonthStr = "0"+birth.monthValue.toString()}
-        else {birthMonthStr = birth.monthValue.toString()}
-        if (birth.dayOfMonth < 10) { birthDayOfMonthStr = "0"+birth.dayOfMonth.toString()}
-        else {birthDayOfMonthStr = birth.dayOfMonth.toString()}
+    private fun getDays(editViewInput: String): String {
+        // Create LocalDateTime object for the birthday
+        val birth = LocalDateTime.parse(editViewInput + "T00:00:00.0400")
 
-        nextBirthday = LocalDateTime.parse(
+        val today = LocalDateTime.now()
+
+        // https://github.com/microsoft/TypeScript/issues/20743
+        // Find the year of next-birth-day
+        val nextBirthdayYear = if (
+            ((today.monthValue == birth.monthValue) && (today.dayOfMonth >= birth.dayOfMonth))
+            || (today.monthValue > birth.monthValue)) {
+            today.year + 1
+        } else {
+            today.year
+        }
+        // Find the month of next-birth-day, and make string Eg., 07 for July
+        val birthMonthStr = if (birth.monthValue < 10) {
+            "0"+birth.monthValue.toString()
+        } else {
+            birth.monthValue.toString()
+        }
+        // Make the day-of-month string for parsing, Eg., "05" for the 5th day of the month. Just "5" causes parsing exception
+        val birthDayOfMonthStr = if (birth.dayOfMonth < 10) {
+            "0"+birth.dayOfMonth.toString()
+        } else {
+            birth.dayOfMonth.toString()
+        }
+
+        // Just add time string format for the YYYY-MM-DD format and parse it to make a LocalDateTime object
+        val nextBirthday = LocalDateTime.parse(
             nextBirthdayYear.toString() + "-"
             + birthMonthStr + "-"
             + birthDayOfMonthStr + "T00:00:00.0400")
+
+        // Find the days till next birthday and return the result string
         val result = ChronoUnit.DAYS.between(today, nextBirthday)
         return result.toString()
     }
